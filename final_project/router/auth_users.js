c1e7845c0726bb1912a36687f0c89aa4fbc12551
NathanDassy <nathandassy@hotmail.fr>
2025-06-12 15:56:04 +0200

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -50,7 +50,7 @@ regd_users.post("/login", (req,res) => {
         }
         return res.status(200).send("User Logged in ")
     } else {
-        return res.status(2082.json({message: " Invalid Login"})
+        return res.status(208).json({message: " Invalid Login"})
         }
 });
 
